c2c77c2f96bec29690f1d9d6516d892c833d71df
Mohammad Farzan <m2_farzan@yahoo.com>
2021-01-17 21:43:52 +0330

    Fix typo in dbconf script

--- utils/dbconfig/initialize-db-dockerized.sh
+++ utils/dbconfig/initialize-db-dockerized.sh
@@ -2,7 +2,7 @@
 
 sudo docker build -t elmos-initialize-db $ELMOS/utils/dbconfig/
 sudo docker create --name ${PROJECT_BASENAME}-1 --network container:${PROJECT_BASENAME}-db -e "DB_USER=$DB_USER" -e "DB_PASS=$DB_PASS" -e "DB_DATABASE=$DB_DATABASE" elmos-initialize-db
-sudo docker cp groups.json ${PROJECT_BASENAME}-1:/i
+sudo docker cp groups.json ${PROJECT_BASENAME}-1:/
 sudo docker cp comgps.json ${PROJECT_BASENAME}-1:/
 sudo docker start --interactive ${PROJECT_BASENAME}-1
 sudo docker rm ${PROJECT_BASENAME}-1
